3c2e8a52530abe87156343f4d22ee24931b71142
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2023-12-09 19:17:05 -0600

    first commit

---
+++ helloworld.dart
@@ -0,0 +1,10 @@
+void main() {
+  print("Hola Dart!");
+  var helloDart = "hola dart!";
+  print(helloDart);
+  var myInt = 8;
+  myInt = myInt + 10;
+  print(myInt);
+  var mydouble = 7.8;
+  print(mydouble);
+}
